dc2e65481469cbb71865f3544a6d5cf930fc61cc
MaximilianoDanielGarcia <maxi.garcia11@hotmail.com>
2025-11-09 15:29:39 -0300

    Setup + navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import { navLinks } from "../lib/constants"
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple logo" />
+
+                <ul>
+                    {
+                        navLinks.map((link, index) => (
+                            <li key={index}>
+                                <a href={link.label}>{link.label}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                <div className="flex items-center gap-2">
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
